f035d8b4c688484c35b01d97004a3eb4483b629a
John <81686492+Explorergt92@users.noreply.github.com>
2023-05-11 13:39:33 -0400

    added docstrings, formatted with black

--- chat.py
+++ chat.py
@@ -1,3 +1,7 @@
+"""
+This module demonstrates the usage of os, openai, and streamlit libraries.
+"""
+
 import os
 import openai
 import streamlit as st
@@ -18,22 +22,39 @@ TEMP_AUDIO_PATH = "temp_audio.wav"
 AUDIO_FORMAT = "audio/wav"
 
 # Load environment variables from .env file and return the keys
-openai.api_key = os.environ.get('OPENAI_API_KEY')
-eleven_api_key = os.environ.get('ELEVEN_API_KEY')
-active_loop_data_set_path = os.environ.get('DEEPLAKE_DATASET_PATH')
+openai.api_key = os.environ.get("OPENAI_API_KEY")
+eleven_api_key = os.environ.get("ELEVEN_API_KEY")
+active_loop_data_set_path = os.environ.get("DEEPLAKE_DATASET_PATH")
 
-# Load embeddings and DeepLake database
+
 def load_embeddings_and_database(active_loop_data_set_path):
+    """
+    Load embeddings and DeepLake database from the given path.
+
+    Args:
+        active_loop_data_set_path (str): The path to the active loop data set.
+
+    Returns:
+        tuple: A tuple containing the embeddings and the DeepLake database.
+    """
     embeddings = OpenAIEmbeddings()
     db = DeepLake(
         dataset_path=active_loop_data_set_path,
         read_only=True,
-        embedding_function=embeddings
+        embedding_function=embeddings,
     )
     return db
 
-# Transcribe audio using OpenAI Whisper API
+
 def transcribe_audio(audio_file_path, openai_key):
+    """
+    Transcribe audio using OpenAI Whisper API.
+
+    :param audio_file_path: The path to the audio file to be transcribed.
+    :type audio_file_path: str
+    :param openai_key: The API key for OpenAI.
+    :type openai_key: str
+    """
     openai.api_key = openai_key
     try:
         with open(audio_file_path, "rb") as audio_file:
@@ -43,8 +64,14 @@ def transcribe_audio(audio_file_path, openai_key):
         print(f"Error calling Whisper API: {str(e)}")
         return None
 
-# Record audio using audio_recorder and transcribe using transcribe_audio
+
 def record_and_transcribe_audio():
+    """
+    Record audio using audio_recorder and transcribe using transcribe_audio.
+
+    Returns:
+        transcription (str): The transcribed text from the recorded audio.
+    """
     audio_bytes = audio_recorder()
     transcription = None
     if audio_bytes:
@@ -60,47 +87,91 @@ def record_and_transcribe_audio():
 
     return transcription
 
-# Display the transcription of the audio on the app
+
 def display_transcription(transcription):
+    """
+    Display the transcription of the audio on the app.
+
+    Args:
+        transcription (str): The transcribed text from the recorded audio.
+    """
     if transcription:
         st.write(f"Transcription: {transcription}")
-        with open("audio_transcription.txt", "w+") as f:
+        with open("audio_transcription.txt", "w+", encoding='utf-8') as f:
             f.write(transcription)
     else:
         st.write("Error transcribing audio.")
 
-# Get user input from Streamlit text input field
+
 def get_user_input(transcription):
+    """
+    Get user input from Streamlit text input field.
+
+    Args:
+        transcription (str): The transcribed text from the recorded audio.
+
+    Returns:
+        str: The user input text from the Streamlit text input field.
+    """
     return st.text_input("", value=transcription if transcription else "", key="input")
 
-# Search the database for a response based on the user's query
+
 def search_db(user_input, db):
+    """
+    Search the database for a response based on the user's query.
+
+    Args:
+        user_input (str): The user's input query.
+        db (object): The database object to be searched.
+
+    Returns:
+        None
+    """
     print(user_input)
     retriever = db.as_retriever()
-    retriever.search_kwargs['distance_metric'] = 'cos'
-    retriever.search_kwargs['fetch_k'] = 100
-    retriever.search_kwargs['maximal_marginal_relevance'] = True
-    retriever.search_kwargs['k'] = 10
+    retriever.search_kwargs["distance_metric"] = "cos"
+    retriever.search_kwargs["fetch_k"] = 100
+    retriever.search_kwargs["maximal_marginal_relevance"] = True
+    retriever.search_kwargs["k"] = 10
     model = ChatOpenAI(model='gpt-3.5-turbo')
     qa = RetrievalQA.from_llm(model, retriever=retriever, return_source_documents=True)
-    return qa({'query': user_input})
+    return qa({"query": user_input})
 
-# Display conversation history using Streamlit messages
+
 def display_conversation(history):
+    """
+    Display conversation history using Streamlit messages.
+
+    Args:
+        history (dict): A dictionary containing the
+        conversation history with keys "generated" and "past".
+
+    Returns:
+        None
+    """
     for i in range(len(history["generated"])):
         message(history["past"][i], is_user=True, key=str(i) + "_user")
-        message(history["generated"][i],key=str(i))
-        #Voice using Eleven API
-        voice= "Bella"
-        text= history["generated"][i]
-        audio = generate(text=text, voice=voice,api_key=eleven_api_key)
-        st.audio(audio, format='audio/mp3')
+        message(history["generated"][i], key=str(i))
+        # Voice using Eleven API
+        voice = "Bella"
+        text = history["generated"][i]
+        audio = generate(text=text, voice=voice, api_key=eleven_api_key)
+        st.audio(audio, format="audio/mp3")
 
-# Main function to run the app
+
 def main():
+    """
+    Main function to run the JarvisBase Streamlit app.
+
+    Args:
+        None
+
+    Returns:
+        None
+    """
     # Initialize Streamlit app with a title
     st.write("# JarvisBase 🧙")
-   
+
     # Load embeddings and the DeepLake database
     db = load_embeddings_and_database(active_loop_data_set_path)
 
@@ -115,11 +186,11 @@ def main():
         st.session_state["generated"] = ["I am ready to help you"]
     if "past" not in st.session_state:
         st.session_state["past"] = ["Hey there!"]
-        
+
     # Search the database for a response based on user input and update session state
     if user_input:
         output = search_db(user_input, db)
-        print(output['source_documents'])
+        print(output["source_documents"])
         st.session_state.past.append(user_input)
         response = str(output["result"])
         st.session_state.generated.append(response)
@@ -127,6 +198,7 @@ def main():
     # Display conversation history using Streamlit messages
     if st.session_state["generated"]:
         display_conversation(st.session_state)
+
 
 # Run the main function when the script is executed
 if __name__ == "__main__":
